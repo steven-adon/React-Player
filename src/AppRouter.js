4124a90250eab7a6399c5a8a266ec04da3a46de1
dong <gengyudonga@gmail.com>
2019-09-27 14:50:30 +0800

    qrcode-img

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -95,6 +95,7 @@ class AppRouter extends React.Component {
   }
 
   getUrlKey = (name) => {
+    // eslint-disable-next-line
     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(window.location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
   }
 
